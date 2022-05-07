1925088bcdc69292f1a28a81813e0df90c8cc46d
KrutarthZala03 <105093250+KrutarthZala03@users.noreply.github.com>
2022-05-07 09:50:01 +0530

    Add files via upload

---
+++ Fibbonacci.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Fibbonacci {
+
+	public static void main (String args[])
+	{
+		
+		int n;
+		System.out.println("Enter starting number of fibbonacci series : ");
+		Scanner kz = new Scanner(System.in);
+		n = kz.nextInt();
+		
+		int t1=0 , t2=1;
+		
+		for(int i = 1 ; i <= n + 5; i++)
+		{
+			if( t1 >= n) {
+				System.out.print(t1 + " ");
+			}
+				int t3 = t1 + t2;
+				t1 = t2;
+				t2 = t3;
+		}
+		
+		
+	}
+}
