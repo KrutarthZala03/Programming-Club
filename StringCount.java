99a9f1143fd447049bbebab7cd5fb8bd9fb38c20
KrutarthZala03 <105093250+KrutarthZala03@users.noreply.github.com>
2022-05-07 11:50:38 +0530

    Add files via upload

---
+++ StringCount.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+public class StringCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner kz = new Scanner(System.in);
+		System.out.println("Enter String : ");
+		String s = kz.nextLine();
+		s = s.trim();
+		int num=0,space = 0,charc = 0;
+		
+		for ( int i = 0 ; i < s.length(); i++ )
+		{
+			if (s.charAt(i) <= '9' && s.charAt(i) >= '0'  )
+			{
+				num++;
+			}
+			else if ( s.charAt(i) == ' ' && s.charAt(i + 1) != ' ')
+			{
+				space++;
+			}
+			else if ( s.charAt(i) >= 'a' && s.charAt(i) <= 'z' || s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')
+			{
+				charc++;
+			}
+		}
+		
+		System.out.println("The numbers in this string are : " + num);
+		System.out.println("The words in this string are : " +(space + 1));
+		System.out.println("The characters in this string are : " + charc);
+		
+		
+	}
+
+}
