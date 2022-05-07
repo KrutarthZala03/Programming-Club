37a853076620b211d66703bdee8b929556936d41
KrutarthZala03 <105093250+KrutarthZala03@users.noreply.github.com>
2022-05-07 09:57:45 +0530

    Update Fibbonacci.java

--- Fibbonacci.java
+++ Fibbonacci.java
@@ -10,12 +10,14 @@ public class Fibbonacci {
 		Scanner kz = new Scanner(System.in);
 		n = kz.nextInt();
 		
-		int t1=0 , t2=1;
+		int t1=0 , t2=1, count = 1;
 		
 		for(int i = 1 ; i <= n + 5; i++)
 		{
-			if( t1 >= n) {
+			if( t1 >= n && count <= 5) 
+			{
 				System.out.print(t1 + " ");
+				count++;
 			}
 				int t3 = t1 + t2;
 				t1 = t2;
